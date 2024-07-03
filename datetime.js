e8938e120f5a354e218b8b7258a9081762ebd2ee
Akintoye-pelumi <p2l5mi@gmail.com>
2024-07-03 23:35:23 +0100

    aa

---
+++ datetime.js
@@ -0,0 +1,13 @@
+function updateDateTime() {
+    const now = new Date();
+    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const day = daysOfWeek[now.getUTCDay()];
+    const hours = now.getUTCHours().toString().padStart(2, '0');
+    const minutes = now.getUTCMinutes().toString().padStart(2, '0');
+    const seconds = now.getUTCSeconds().toString().padStart(2, '0');
+    const time = `${hours}:${minutes}:${seconds} UTC`;
+    document.getElementById('datetime').innerText = `${day}, ${time}`;
+}
+
+setInterval(updateDateTime, 1000);
+updateDateTime();
